77cf39458aa49233db007475a3b2e64c9429dcf9
BibhutiSingh <bibhuti.nath@outlook.com>
2019-04-26 04:19:57 +0530

    Base Version

---
+++ DeltaX.App/Entities/Producer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeltaX.App.Entities
+{
+    public class Producer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Gender Sex { get; set; }
+        public DateTime DOB { get; set; }
+        public string Bio { get; set; }
+    }
+}
